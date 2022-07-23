035ce7db55ce2c9e1ce89c38d0f76f2a6d4a595d
noctera <noctera.dev@gmail.com>
2022-07-24 01:29:18 +0200

    cmake linux support

--- cmake/emscripten.cmake
+++ cmake/emscripten.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
     emscripten
     GIT_REPOSITORY https://github.com/emscripten-core/emsdk.git
-    GIT_TAG        961e66c5099e1119dda25f7a8945d155fc561c90    
+    GIT_TAG        961e66c5099e1119dda25f7a8945d155fc561c90
 )
 
 FetchContent_MakeAvailable(emscripten)
@@ -13,7 +13,7 @@ add_custom_target(
     emscripten_setup
     ALL
     WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/_deps/emscripten-src"
-    #COMMAND "${CMAKE_BINARY_DIR}/_deps/emscripten-src/emsdk" install latest
+    COMMAND "${CMAKE_BINARY_DIR}/_deps/emscripten-src/emsdk" install latest
     COMMAND "${CMAKE_BINARY_DIR}/_deps/emscripten-src/emsdk" activate latest
     COMMAND_EXPAND_LISTS
 )
