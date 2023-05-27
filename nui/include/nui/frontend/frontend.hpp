324f459b5db8cc433e6c8ef75252e7461f7b3af6
Tim Ebbeke <tim06tr@gmail.com>
2023-05-27 03:48:25 +0200

    Added more kitchen sink headers.

---
+++ nui/include/nui/frontend/frontend.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <nui/frontend/attributes.hpp>
+#include <nui/frontend/components.hpp>
+#include <nui/frontend/dom.hpp>
+#include <nui/frontend/elements.hpp>
+#include <nui/frontend/generator_typedefs.hpp>
+#include <nui/frontend/rpc_client.hpp>
+#include <nui/frontend/val.hpp>
