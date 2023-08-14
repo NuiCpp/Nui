d4e9f26c9b4038d3b089caeb2f6f130978d026ed
Tim.Ebbeke <Tim.Ebbeke@sennheiser.com>
2023-08-14 16:34:34 +0200

    Fixed dependency files.

--- cmake/dependencies/fmt.cmake
+++ cmake/dependencies/fmt.cmake
@@ -4,8 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
     fmt
     GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-    GIT_TAG        5f774c0aed4af6494b412cd3883e0f09295bd79d    
-    FIND_PACKAGE_ARGS NAMES fmt
+    GIT_TAG        5f774c0aed4af6494b412cd3883e0f09295bd79d
 )
 
 FetchContent_MakeAvailable(fmt)
--- cmake/dependencies/portable_file_dialog.cmake
+++ cmake/dependencies/portable_file_dialog.cmake
@@ -5,8 +5,7 @@ include(FetchContent)
 FetchContent_Declare(
     portable_file_dialogs
     GIT_REPOSITORY https://github.com/5cript/portable-file-dialogs.git
-    GIT_TAG        5652fbd0df05f001aa2e92d86c22f762a03c1fd9  
-    FIND_PACKAGE_ARGS  
+    GIT_TAG        5652fbd0df05f001aa2e92d86c22f762a03c1fd9
 )
 
 FetchContent_MakeAvailable(portable_file_dialogs)
