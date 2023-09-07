298c3cf425000e3b36c95c3ce0df139def980cb4
Tim.Ebbeke <Tim.Ebbeke@sennheiser.com>
2023-09-07 18:07:38 +0200

    Improved setAttribute overloads.

--- nui/include/nui/frontend/dom/childless_element.hpp
+++ nui/include/nui/frontend/dom/childless_element.hpp
@@ -49,11 +49,15 @@ namespace Nui::Dom
             if (value)
                 element_.call<Nui::val>("setAttribute", Nui::val{std::string{key}}, Nui::val{""});
         }
-        void setAttribute(std::string_view key, int value)
+        template <typename T>
+        requires std::integral<T>
+        void setAttribute(std::string_view key, T value)
         {
             element_.call<Nui::val>("setAttribute", Nui::val{std::string{key}}, Nui::val{value});
         }
-        void setAttribute(std::string_view key, double value)
+        template <typename T>
+        requires std::floating_point<T>
+        void setAttribute(std::string_view key, T value)
         {
             element_.call<Nui::val>("setAttribute", Nui::val{std::string{key}}, Nui::val{value});
         }
