3383aa15bef515a3ae2ee3c68d49a783ca47570f
Tim.Ebbeke <Tim.Ebbeke@sennheiser.com>
2023-10-23 17:59:20 +0200

    Bumped roar.

--- cmake/dependencies/roar.cmake
+++ cmake/dependencies/roar.cmake
@@ -1,6 +1,6 @@
 option(NUI_FETCH_ROAR "Fetch roar" ON)
 set(NUI_ROAR_REPOSITORY "https://github.com/5cript/roar.git" CACHE STRING "roar repository")
-set(NUI_ROAR_TAG "d76fa5a7386008c32157856e5dcf448ab9600dda" CACHE STRING "roar tag")
+set(NUI_ROAR_TAG "59953158142fdb48f1e36273b16ef34e20fc3ae1" CACHE STRING "roar tag")
 
 if(NUI_FETCH_ROAR)
     include(FetchContent)
