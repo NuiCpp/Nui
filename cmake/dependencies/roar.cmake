aac62f8e5f303b96616a4f6c1f875a8248f73fad
Tim Ebbeke <Tim06TR@gmail.com>
2023-11-05 17:16:49 +0100

    Updated roar.

--- cmake/dependencies/roar.cmake
+++ cmake/dependencies/roar.cmake
@@ -1,6 +1,6 @@
 option(NUI_FETCH_ROAR "Fetch roar" ON)
 set(NUI_ROAR_REPOSITORY "https://github.com/5cript/roar.git" CACHE STRING "roar repository")
-set(NUI_ROAR_TAG "9e60320c7d60f91feb2bc13948340a97dd8ee190" CACHE STRING "roar tag")
+set(NUI_ROAR_TAG "7efcf0bd3d971426e0f6fe793927d4bc0ad49d85" CACHE STRING "roar tag")
 
 if(NUI_FETCH_ROAR)
     include(FetchContent)
