e21953c7a4278c02afa789b3e4a50b598f01a228
5cript <tim06tr@gmail.com>
2024-11-23 22:58:22 +0100

    Removed check for missing member.

--- nui/include/nui/event_system/range_event_context.hpp
+++ nui/include/nui/event_system/range_event_context.hpp
@@ -45,7 +45,7 @@ namespace Nui
             }
             friend bool operator==(RangeStateInterval const& lhs, RangeStateInterval const& rhs)
             {
-                return lhs.low_ == rhs.low_ && lhs.high_ == rhs.high_ && lhs.type_ == rhs.type_;
+                return lhs.low_ == rhs.low_ && lhs.high_ == rhs.high_;
             }
             friend bool operator!=(RangeStateInterval const& lhs, RangeStateInterval const& rhs)
             {
