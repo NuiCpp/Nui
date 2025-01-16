20213a31804f7dc89e0d2330989b070c430114e4
Tim.Ebbeke <Tim.Ebbeke@sennheiser.com>
2025-01-16 10:23:00 +0100

    Updated roar revision.

--- cmake/dependencies/roar.cmake
+++ cmake/dependencies/roar.cmake
@@ -1,6 +1,6 @@
 option(NUI_FETCH_ROAR "Fetch roar" ON)
 set(NUI_ROAR_REPOSITORY "https://github.com/5cript/roar.git" CACHE STRING "roar repository")
-set(NUI_ROAR_TAG "f1ff8911bdc1d49bc118def679638227337a0e20" CACHE STRING "roar tag")
+set(NUI_ROAR_TAG "ed28f3c14a47d75c57464dbcf29e60a59dcfd534" CACHE STRING "roar tag")
 
 if(NUI_FETCH_ROAR)
     include(FetchContent)
