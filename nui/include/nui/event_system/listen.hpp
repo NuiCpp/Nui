0482b96fb759933c366eb371810d9701ddf669f7
Tim Ebbeke <Tim06TR@gmail.com>
2025-02-05 01:18:34 +0100

    Fixed possible infinite recursion in listen overloads.

--- nui/include/nui/event_system/listen.hpp
+++ nui/include/nui/event_system/listen.hpp
@@ -6,11 +6,36 @@
 #include <utility>
 #include <memory>
 #include <functional>
+#include <type_traits>
 
 namespace Nui
 {
+    namespace Detail
+    {
+        template <typename>
+        struct IsStdFunction : std::false_type
+        {};
+        template <typename RetT, typename... ArgsT>
+        struct IsStdFunction<std::function<RetT(ArgsT...)>> : std::true_type
+        {};
+    }
+
     template <typename ValueT>
     void listen(EventContext& eventContext, Observed<ValueT> const& obs, std::function<bool(ValueT const&)> onEvent)
+    {
+        const auto eventId = eventContext.registerEvent(Event{
+            [obs = Detail::CopyableObservedWrap{obs}, onEvent = std::move(onEvent)](auto) {
+                return onEvent(obs.value());
+            },
+            []() {
+                return true;
+            }});
+        obs.attachEvent(eventId);
+    }
+
+    template <typename ValueT>
+    requires std::is_scalar_v<ValueT>
+    void listen(EventContext& eventContext, Observed<ValueT> const& obs, std::function<bool(ValueT)> onEvent)
     {
         const auto eventId = eventContext.registerEvent(Event{
             [obs = Detail::CopyableObservedWrap{obs}, onEvent = std::move(onEvent)](auto) {
@@ -31,10 +56,23 @@ namespace Nui
         });
     }
 
-    template <typename ValueT, typename FunctionT>
-    void listen(EventContext& eventContext, Observed<ValueT> const& obs, FunctionT onEvent)
+    template <typename ValueT>
+    requires std::is_scalar_v<ValueT>
+    void listen(EventContext& eventContext, Observed<ValueT> const& obs, std::function<void(ValueT)> onEvent)
     {
-        return listen(eventContext, obs, std::function(std::move(onEvent)));
+        return listen(eventContext, obs, [onEvent = std::move(onEvent)](ValueT value) {
+            onEvent(value);
+            return true;
+        });
+    }
+
+    template <typename ValueT, typename FunctionT>
+    requires(
+        (std::invocable<FunctionT, ValueT const&> || std::invocable<FunctionT, ValueT>) &&
+        !Detail::IsStdFunction<FunctionT>::value)
+    void listen(EventContext& eventContext, Observed<ValueT> const& obs, FunctionT&& onEvent)
+    {
+        return listen(eventContext, obs, std::function(std::forward<FunctionT>(onEvent)));
     }
 
     template <typename ValueT>
@@ -42,6 +80,25 @@ namespace Nui
         EventContext& eventContext,
         std::shared_ptr<Observed<ValueT>> const& obs,
         std::function<bool(ValueT const&)> onEvent)
+    {
+        const auto eventId = eventContext.registerEvent(Event{
+            [weak = std::weak_ptr<Observed<ValueT>>{obs}, onEvent = std::move(onEvent)](auto) {
+                if (auto obs = weak.lock(); obs)
+                    return onEvent(obs->value());
+                return false;
+            },
+            [weak = std::weak_ptr<Observed<ValueT>>{obs}]() {
+                return !weak.expired();
+            }});
+        obs->attachEvent(eventId);
+    }
+
+    template <typename ValueT>
+    requires std::is_scalar_v<ValueT>
+    void listen(
+        EventContext& eventContext,
+        std::shared_ptr<Observed<ValueT>> const& obs,
+        std::function<bool(ValueT)> onEvent)
     {
         const auto eventId = eventContext.registerEvent(Event{
             [weak = std::weak_ptr<Observed<ValueT>>{obs}, onEvent = std::move(onEvent)](auto) {
@@ -67,21 +124,43 @@ namespace Nui
         });
     }
 
-    template <typename ValueT, typename FunctionT>
-    void listen(EventContext& eventContext, std::shared_ptr<Observed<ValueT>> const& obs, FunctionT onEvent)
+    template <typename ValueT>
+    requires std::is_scalar_v<ValueT>
+    void listen(
+        EventContext& eventContext,
+        std::shared_ptr<Observed<ValueT>> const& obs,
+        std::function<void(ValueT)> onEvent)
     {
-        return listen(eventContext, obs, std::function(std::move(onEvent)));
+        return listen(eventContext, obs, [onEvent = std::move(onEvent)](ValueT value) {
+            onEvent(value);
+            return true;
+        });
     }
 
     template <typename ValueT, typename FunctionT>
-    void listen(std::shared_ptr<Observed<ValueT>> const& obs, FunctionT onEvent)
+    requires(
+        (std::invocable<FunctionT, ValueT const&> || std::invocable<FunctionT, ValueT>) &&
+        !Detail::IsStdFunction<FunctionT>::value)
+    void listen(EventContext& eventContext, std::shared_ptr<Observed<ValueT>> const& obs, FunctionT&& onEvent)
     {
-        return listen(globalEventContext, obs, std::function(std::move(onEvent)));
+        return listen(eventContext, obs, std::function(std::forward<FunctionT>(onEvent)));
     }
 
     template <typename ValueT, typename FunctionT>
-    void listen(Observed<ValueT> const& obs, FunctionT onEvent)
+    requires(
+        (std::invocable<FunctionT, ValueT const&> || std::invocable<FunctionT, ValueT>) &&
+        !Detail::IsStdFunction<FunctionT>::value)
+    void listen(std::shared_ptr<Observed<ValueT>> const& obs, FunctionT&& onEvent)
     {
-        return listen(globalEventContext, obs, std::function(std::move(onEvent)));
+        return listen(globalEventContext, obs, std::function(std::forward<FunctionT>(onEvent)));
+    }
+
+    template <typename ValueT, typename FunctionT>
+    requires(
+        (std::invocable<FunctionT, ValueT const&> || std::invocable<FunctionT, ValueT>) &&
+        !Detail::IsStdFunction<FunctionT>::value)
+    void listen(Observed<ValueT> const& obs, FunctionT&& onEvent)
+    {
+        return listen(globalEventContext, obs, std::function(std::forward<FunctionT>(onEvent)));
     }
 }
