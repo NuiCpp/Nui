590f1636d5725c1579a1f25017dc1364ef130472
5cript <tim06tr@gmail.com>
2026-02-27 01:02:06 +0100

    Bumped binaryen version.

--- cmake/dependencies/binaryen.cmake
+++ cmake/dependencies/binaryen.cmake
@@ -1,9 +1,9 @@
 option(NUI_FETCH_BINARYEN "Fetch binaryen" ON)
 
 if (WIN32)
-    set(NUI_BINARYEN_URL "https://github.com/WebAssembly/binaryen/releases/download/version_114/binaryen-version_114-x86_64-windows.tar.gz" CACHE STRING "Binaryen url")
+    set(NUI_BINARYEN_URL "https://github.com/WebAssembly/binaryen/releases/download/version_124/binaryen-version_124-x86_64-windows.tar.gz" CACHE STRING "Binaryen url")
 elseif(UNIX)
-    set(NUI_BINARYEN_URL "https://github.com/WebAssembly/binaryen/releases/download/version_114/binaryen-version_114-x86_64-linux.tar.gz" CACHE STRING "Binaryen url")
+    set(NUI_BINARYEN_URL "https://github.com/WebAssembly/binaryen/releases/download/version_124/binaryen-version_124-x86_64-linux.tar.gz" CACHE STRING "Binaryen url")
 endif()
 
 if (NUI_FETCH_BINARYEN)
