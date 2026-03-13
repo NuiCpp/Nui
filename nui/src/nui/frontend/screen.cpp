fc948519fdda33481e13a3ba0cfe3e6ac72a96f1
5cript <tim06tr@gmail.com>
2026-03-13 18:05:24 +0100

    Eliminated conversion warning.

--- nui/src/nui/frontend/screen.cpp
+++ nui/src/nui/frontend/screen.cpp
@@ -15,18 +15,17 @@ namespace Nui
         RpcClient::getRemoteCallableWithBackChannel(
             "Nui::Screen::getDisplays", [callback = std::move(callback)](Nui::val response) {
                 std::vector<Display> result;
-                const auto length = response["length"].as<int32_t>();
-                result.reserve(length);
-                for (int32_t i = 0; i < length; ++i)
+                const auto length = response["length"].as<std::int32_t>();
+                result.reserve(static_cast<std::size_t>(length));
+                for (std::int32_t i = 0; i < length; ++i)
                 {
-                    result.push_back(
-                        Display{
-                            response[i]["x"].as<int32_t>(),
-                            response[i]["y"].as<int32_t>(),
-                            response[i]["width"].as<int32_t>(),
-                            response[i]["height"].as<int32_t>(),
-                            response[i]["isPrimary"].as<bool>(),
-                            response[i]["deviceName"].as<std::string>()});
+                    result.emplace_back(
+                        response[i]["x"].as<std::int32_t>(),
+                        response[i]["y"].as<std::int32_t>(),
+                        response[i]["width"].as<std::int32_t>(),
+                        response[i]["height"].as<std::int32_t>(),
+                        response[i]["isPrimary"].as<bool>(),
+                        response[i]["deviceName"].as<std::string>());
                 }
                 callback(std::move(result));
             })();
@@ -37,10 +36,10 @@ namespace Nui
             "Nui::Screen::getPrimaryDisplay", [callback = std::move(callback)](Nui::val response) {
                 callback(
                     Display{
-                        response["x"].as<int32_t>(),
-                        response["y"].as<int32_t>(),
-                        response["width"].as<int32_t>(),
-                        response["height"].as<int32_t>(),
+                        response["x"].as<std::int32_t>(),
+                        response["y"].as<std::int32_t>(),
+                        response["width"].as<std::int32_t>(),
+                        response["height"].as<std::int32_t>(),
                         response["isPrimary"].as<bool>(),
                         response["deviceName"].as<std::string>()});
             })();
