73d9d6036353a193691e16b8116e60b8f354e469
5cript <tim06tr@gmail.com>
2026-03-24 00:56:54 +0100

    Improved OnDestroy class.

--- nui/include/nui/shared/on_destroy.hpp
+++ nui/include/nui/shared/on_destroy.hpp
@@ -7,19 +7,19 @@ namespace Nui
     class OnDestroy
     {
       public:
-        OnDestroy(std::function<void()> onDestroy)
+        explicit OnDestroy(std::function<void()> onDestroy)
             : wasMoved_{false}
             , onDestroy_{std::move(onDestroy)}
         {}
         OnDestroy(OnDestroy const&) = delete;
-        OnDestroy(OnDestroy&& other)
+        OnDestroy(OnDestroy&& other) noexcept
             : wasMoved_{other.wasMoved_}
             , onDestroy_{std::move(other.onDestroy_)}
         {
             other.wasMoved_ = true;
         }
         OnDestroy& operator=(OnDestroy const&) = delete;
-        OnDestroy& operator=(OnDestroy&& other)
+        OnDestroy& operator=(OnDestroy&& other) noexcept
         {
             wasMoved_ = other.wasMoved_;
             onDestroy_ = std::move(other.onDestroy_);
