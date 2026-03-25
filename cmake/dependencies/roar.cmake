929067f18af7ec9f20485aa4f36a45f8042d667b
5cript <tim06tr@gmail.com>
2026-03-25 21:18:57 +0100

    Bumped roar tag to unit test fixed version.

--- cmake/dependencies/roar.cmake
+++ cmake/dependencies/roar.cmake
@@ -1,6 +1,6 @@
 option(NUI_FETCH_ROAR "Fetch roar" ON)
 set(NUI_ROAR_REPOSITORY "https://github.com/5cript/roar.git" CACHE STRING "roar repository")
-set(NUI_ROAR_TAG "a787bce9c8132f4c860bc9e55bff742fd1a3276f" CACHE STRING "roar tag")
+set(NUI_ROAR_TAG "52acf8675404d6e370a4f7f6ce8e78d507b18e5d" CACHE STRING "roar tag")
 
 if(NUI_FETCH_ROAR)
     include(FetchContent)
